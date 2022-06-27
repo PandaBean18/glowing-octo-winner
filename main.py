0785d1b8106e4e37ae9125603ee9560fbe83db44
raghavbahukhandi <raghapb34n@gmail.com>
2022-06-27 23:24:02 +0530

    as always, not so initial commit

---
+++ main.py
@@ -0,0 +1,64 @@
+from flask import Flask 
+from markupsafe import escape
+from flask import render_template
+from flask import redirect
+from flask import request
+from flask import url_for
+from flask import session
+from users import *
+
+app = Flask(__name__)
+app.secret_key = b'3bbeb23b5e850898a4b872c5908302899d3fa8f63cda539cd2ce03205531346a'
+
+def current_user():
+    if session.get('session_token'): 
+        return User.find_by_session_token(session.get('session_token'))
+    else: 
+        return None 
+
+@app.route("/users/<id>")
+def users_show(id):
+    user = User.find_by_id(int(id))
+    if user: 
+        return f"<p>Hello, {user.username}</p>"
+    else: 
+        return "<p>Hello, World!</p>"
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/users/new", methods=['POST', 'GET'])
+def new_user():
+    if request.method == 'GET':
+        return render_template('new_user.html')
+    else: 
+        username = request.form['user[name]']
+        password = request.form['user[pass]']
+
+        user = User(username, password).create()
+        return redirect(f"/users/{user.id}")
+
+@app.route("/login", methods=['POST', 'GET'])
+def login(): 
+    if current_user() == None:
+        if request.method == 'GET':
+            return render_template('login.html')
+        else: 
+            username = request.form['user[username]']
+            password = request.form['user[pass]']
+
+            user = User.find_by_credentials(username, password)
+
+            if user: 
+                session_token = user.reset_session_token()
+                session['session_token'] = session_token
+                return users_show(user.id)
+    else: 
+        return users_show(current_user().id)
+
+@app.route("/logout")
+def logout(): 
+    session['session_token'] = None 
+    return redirect('/login')
+
---
+++ users.py
@@ -0,0 +1,285 @@
+# id: integer, unique 
+# username: string, unique 
+# password_digest: string, unique*
+# session_token: string
+
+import pickle 
+import secrets
+import bcrypt
+import time
+from datetime import date 
+
+class User: 
+
+    # returns all the users in the users.dat file
+    # all the objects returned by this method are instances of the User class
+    def all():
+        read_obj = open('users.dat', 'rb')
+        data = [] 
+
+        while True: 
+            try: 
+                rec = pickle.load(read_obj)
+                data.append(rec)
+            except: 
+                break 
+        read_obj.close()
+
+        users = []
+
+        for rec in data:
+            id = rec['id']
+            username = rec['username']
+            password_digest = rec['password_digest']
+            session_token = rec['session_token']
+            created_on = rec['created_on']
+            user = User(username, None, password_digest, session_token, created_on, id)
+            users.append(user)
+
+        return users 
+
+    # updating an existing value in the users.dat file
+    # attributes is a hash 
+    def update(id, attributes): 
+        read_obj = open('users.dat', 'rb')
+        data = [] 
+        record_updated = False 
+
+        while not record_updated: 
+            try: 
+                rec = pickle.load(read_obj)
+
+                if rec['id'] == id:
+                    # if the field is there in attributes hash then it is to be updated (represented here by `attributes.get('username)`)
+                    # else we will use the existing value (represented here by `rec.get('username)`))
+                    rec['username'] = attributes.get('username') or rec.get('username') # using dict#get as it returns nil instead of error
+                    rec['session_token'] = attributes.get('session_token') or rec.get('session_token')
+
+                data.append(rec)
+                record_updated = True 
+            except:
+                break 
+
+        read_obj.close()
+        # writing all the data back in the file 
+
+        write_obj = open('users.dat', 'wb')
+
+        for rec in data:
+            pickle.dump(rec, write_obj)
+
+        write_obj.close()
+
+        return record_updated
+
+    def destroy(id):
+        read_obj = open('users.dat', 'rb')
+        data = [] 
+
+        while True: 
+            try: 
+                rec = pickle.load(read_obj)
+                data.append(rec)
+            except: 
+                break 
+        read_obj.close()
+
+        write_obj = open('users.dat', 'wb')
+
+        for rec in data: 
+            if rec['id'] != id:
+                pickle.dump(rec, write_obj)
+
+        write_obj.close()
+
+
+    # finding user with id
+    def find_by_id(id):
+        read_obj = open('users.dat', 'rb')
+
+        while True: 
+            try: 
+                rec = pickle.load(read_obj)
+                if rec['id'] == id: 
+                    read_obj.close()
+                    username = rec['username']
+                    password_digest = rec['password_digest']
+                    session_token = rec['session_token']
+                    created_on = rec['created_on']
+                    return User(username, None, password_digest, session_token, created_on, id)
+            except:
+                break 
+        read_obj.close()
+        return None
+
+    def find_by_session_token(session_token):
+        read_obj = open('users.dat', 'rb')
+
+        while True: 
+            try:
+                rec = pickle.load(read_obj)
+                if rec['session_token'] == session_token:
+                    read_obj.close()
+                    id = rec['id']
+                    username = rec['username']
+                    password_digest = rec['password_digest']
+                    session_token = rec['session_token']
+                    created_on = rec['created_on']
+                    return User(username, None, password_digest, session_token, created_on, id)
+            except: 
+                break 
+        
+        read_obj.close()
+        return None
+
+    def find_by_credentials(username, password):
+        read_obj = open('users.dat', 'rb')
+
+        while True: 
+            try: 
+                rec = pickle.load(read_obj)
+                if rec['username'] == username:
+                    read_obj.close()
+                    id = rec['id']
+                    username = rec['username']
+                    password_digest = rec['password_digest']
+                    session_token = rec['session_token']
+                    created_on = rec['created_on']
+                    return User(username, None, password_digest, session_token, created_on, id)
+                    if user.check_password(password):
+                        return user 
+                    else: 
+                        return None 
+            except: 
+                break 
+            
+        read_obj.close()
+        return None
+                
+    def __init__(self, username, password, password_digest=None, session_token=None, created_on=None, id=None):
+        if id:
+            self.id = int(id)
+        else:
+            self.id =  None
+
+        self.username = username 
+        self.password = password
+        self.password_digest = password_digest or self.__create_password_digest(password) # if a password digest is sent through
+                                                                                          # from User#all
+
+        if type(self.password_digest) != bytes: # ensuring that the password digest is only sent through by a class function
+            self.password_digest = None 
+
+        self.session_token = session_token
+        self.created_on = created_on
+        self.errors = []
+
+    def create(self):
+        if not (self.valid()):
+            return self.errors 
+        else: 
+            last_id = self.__get_last_user_id()
+            id = last_id + 1
+            username = self.username 
+            password_digest = self.password_digest 
+            session_token = self.__generate_session_token()
+            created_on = date.today().strftime("%d %B, %Y")
+
+            h = {
+                'id': id,
+                'username': username, 
+                'password_digest': password_digest, 
+                'session_token': session_token,
+                'created_on': created_on
+                }
+
+            write_obj = open('users.dat', 'ab')
+
+            pickle.dump(h, write_obj)
+
+            write_obj.close()
+
+            return User(username, None, password_digest, session_token, id)
+
+    def reset_session_token(self):
+        session_token = self.__generate_session_token() 
+        User.update(self.id, {'session_token': session_token})
+        return session_token
+
+    # ensuring that all the values passed during the initialization of the class are valid
+    def valid(self): 
+        if type(self.username) != str: 
+            self.errors.append('Invalid username.') 
+            return False
+        elif type(self.password) != str: 
+            self.errors.append('Invalid password.')
+            return False 
+        elif len(self.password) < 6: 
+            self.errors.append('Password is too short.')
+            return False 
+        elif not self.__ensure_unique_username(): 
+            self.errors.append('Username already taken.')
+            return False 
+
+        return True 
+
+    def check_password(self, password):
+        return bcrypt.checkpw(bytes(password, 'utf-8'), self.password_digest)
+
+
+    def __create_password_digest(self, password):
+        b_password = bytes(password, 'utf-8') # encoding the password 
+        hashed_password = bcrypt.hashpw(b_password, bcrypt.gensalt())
+        return hashed_password
+
+    def __ensure_unique_username(self): # returns false if username is taken 
+        users = User.all() 
+
+        for user in users: 
+            if user.username == self.username: 
+                return False 
+        
+        return True 
+
+    def __get_last_user_id(self):
+        read_obj = open('users.dat', 'rb')
+        i = 0
+        read_obj.seek(0, 2)
+
+        while True: 
+            try: 
+                rec = pickle.load(read_obj)
+                break 
+            except: 
+                try: 
+                    i -= 1
+                    read_obj.seek(i, 2)
+                except: 
+                    read_obj.close()
+                    return 0
+
+        read_obj.close() 
+        print(rec)
+        return rec['id']
+
+    def __generate_session_token(self):
+        return secrets.token_urlsafe(16)
+
+# User('TestUser1', 'TestUser1Pass').create()
+# time.sleep(0.1)
+# User('TestUser2', 'TestUser2Pass').create()
+# time.sleep(0.1)
+# User('TestUser3', 'TestUser3Pass').create()
+# time.sleep(0.1)
+# User('TestUser4', 'TestUser4Pass').create()
+
+u = User.all()
+
+for x in u: 
+    print(x.id)
+    print(x.username)
+
+# User.destroy(1)
+# User.destroy(2)
+# User.destroy(3)
+# User.destroy(4)
